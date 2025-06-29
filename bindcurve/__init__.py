a992c249eb6768bbdab9ead39899ad3074be63e6
choutkaj <choutka.jan@gmail.com>
2025-06-29 14:19:05 +0200

    gitignore

--- bindcurve/__init__.py
+++ bindcurve/__init__.py
@@ -1,10 +1,18 @@
 from bindcurve.data import load_csv, load_df, plot, plot_grid, plot_asymptotes, plot_traces, plot_value, report
 from bindcurve.calculate import fit_50, fit_Kd_direct, fit_Kd_competition, convert
-import bindcurve.models as models
+from bindcurve.models import IC50, logIC50,dir_simple, dir_specific, dir_total, comp_3st_specific, comp_3st_total, comp_4st_specific, comp_4st_total, cheng_prusoff, cheng_prusoff_corr, coleska
 from bindcurve._version import __version__
 
 
 __all__ = [
     "load_csv", "load_df", "plot", "plot_grid", "plot_asymptotes", "plot_traces", "plot_value", "report",
-    "fit_50", "fit_Kd_direct", "fit_Kd_competition", "convert", "models", "__version__"
+    "fit_50", "fit_Kd_direct", "fit_Kd_competition", "convert", "IC50", "logIC50", "dir_simple", "dir_specific", "dir_total",
+    "comp_3st_specific", "comp_3st_total", "comp_4st_specific", "comp_4st_total", "cheng_prusoff", "cheng_prusoff_corr", "coleska",
+    "__version__"
 ]
+
+
+
+
+
+
