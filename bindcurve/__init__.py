e46d1ceab9c209957d1277f4053b6db11280c4f6
choutkaj <choutka.jan@gmail.com>
2025-06-29 18:01:49 +0200

    build settings

--- bindcurve/__init__.py
+++ bindcurve/__init__.py
@@ -1,7 +1,6 @@
 from bindcurve.data import load_csv, load_df, plot, plot_grid, plot_asymptotes, plot_traces, plot_value, report
 from bindcurve.calculate import fit_50, fit_Kd_direct, fit_Kd_competition, convert
 from bindcurve.models import IC50, logIC50,dir_simple, dir_specific, dir_total, comp_3st_specific, comp_3st_total, comp_4st_specific, comp_4st_total, cheng_prusoff, cheng_prusoff_corr, coleska
-from bindcurve._version import __version__
 
 
 __all__ = [
@@ -10,7 +9,6 @@ __all__ = [
     "comp_3st_specific", "comp_3st_total", "comp_4st_specific", "comp_4st_total", "cheng_prusoff", "cheng_prusoff_corr", "coleska",
     "__version__"
 ]
-
 
 
 
