521ffce1df78c617e22a672a80f0c7788c3a2cf0
choutkaj <choutka.jan@gmail.com>
2026-04-28 12:43:22 +0200

    Add results package exports

---
+++ bindcurve/results/__init__.py
@@ -0,0 +1,5 @@
+"""Structured fit result containers."""
+
+from bindcurve.results.core import FitMetrics, FitResult, FitResults, ParameterEstimate
+
+__all__ = ["FitMetrics", "FitResult", "FitResults", "ParameterEstimate"]
