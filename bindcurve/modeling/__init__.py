e89ad2d97d5a1f21c18194d467ea751801386eb6
choutkaj <choutka.jan@gmail.com>
2026-04-28 13:20:17 +0200

    Export logistic model family

--- bindcurve/modeling/__init__.py
+++ bindcurve/modeling/__init__.py
@@ -1,8 +1,15 @@
 """Dose-response model definitions and registry."""
 
 from bindcurve.modeling.base import BaseDoseResponseModel
-from bindcurve.modeling.logistic import IC50Model
+from bindcurve.modeling.logistic import EC50Model, IC50Model, LogIC50Model
 from bindcurve.modeling.parameters import ParameterSpec
 from bindcurve.modeling.registry import get_model
 
-__all__ = ["BaseDoseResponseModel", "IC50Model", "ParameterSpec", "get_model"]
+__all__ = [
+    "BaseDoseResponseModel",
+    "EC50Model",
+    "IC50Model",
+    "LogIC50Model",
+    "ParameterSpec",
+    "get_model",
+]
