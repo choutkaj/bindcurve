84098027958e86d7d76c138a0d2a2512d83bd226
choutkaj <choutka.jan@gmail.com>
2026-04-28 11:56:58 +0200

    Add fitting package exports

---
+++ bindcurve/fitting/__init__.py
@@ -0,0 +1,6 @@
+"""Fitting coordination and settings."""
+
+from bindcurve.fitting.calculator import FitCalculator, fit
+from bindcurve.fitting.settings import FitSettings
+
+__all__ = ["FitCalculator", "FitSettings", "fit"]
